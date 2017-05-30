432e06eca8607ad1c6b34d4d1cfa35c6757a318e
Trustet <yannik@trustet.de>
2017-05-30 20:19:43 +0200

    Spiel kann erstellt werden

--- src/local/ui/gui/RisikoClientGUI.java
+++ src/local/ui/gui/RisikoClientGUI.java
@@ -1,6 +1,7 @@
 package local.ui.gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -24,7 +25,9 @@ import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 import local.domain.Spielfeld;
+import local.domain.exceptions.KeinLandZumAngreifenException;
 import local.domain.exceptions.SpielerExistiertBereitsException;
+import local.ui.cui.IO;
 import local.valueobjects.Spieler;
 import net.miginfocom.swing.MigLayout;
 
@@ -38,8 +41,16 @@ public class RisikoClientGUI extends JFrame{
     JLabel spieler4Lab = new JLabel();
     JLabel spieler5Lab = new JLabel();
     JLabel spieler6Lab = new JLabel();
+    JLabel missionen = new JLabel();
+    int anzahl;
     
 	public RisikoClientGUI(){
+	    spieler1Lab.setFont(new Font("Impact", Font.PLAIN,30));
+		spieler2Lab.setFont(new Font("Impact", Font.PLAIN,30));
+		spieler3Lab.setFont(new Font("Impact", Font.PLAIN,30));
+		spieler4Lab.setFont(new Font("Impact", Font.PLAIN,30));
+		spieler5Lab.setFont(new Font("Impact", Font.PLAIN,30));
+		spieler6Lab.setFont(new Font("Impact", Font.PLAIN,30));
 		this.start();
 	}
 	public static void main(String[] args) {
@@ -114,6 +125,7 @@ public class RisikoClientGUI extends JFrame{
 	
 	}
     public void spiel(String name, int anzahl,JFrame frameStart) {
+    	this.anzahl = anzahl;
     	try{
     		sp.erstelleSpieler(name);
 	    	
@@ -148,10 +160,6 @@ public class RisikoClientGUI extends JFrame{
 	        frame.add(panel);
 	        //JTextArea spielfeld = new JTextArea("Weltkarte",30,20);
 	        JTextArea platzhalter = new JTextArea("platzhalter",10,20);
-	        
-	   
-	      
-	        JTextArea missionen = new JTextArea("Missionen",10,20);
 	        JTextArea karten = new JTextArea("Karten",10,20);
 	        JTextArea statistik = new JTextArea("Statistik",15,20);
 	        JButton next = new JButton("Naechste Phase");
@@ -181,7 +189,7 @@ public class RisikoClientGUI extends JFrame{
 	        panel.add(spieler5Lab,"center");
 	        panel.add(spieler6Lab,"center");
 	        panel.add(statistik,"left,top,growy");
-	        panel.add(missionen,"left,split2");
+	        panel.add(missionen,"left,split2,wmin 200,hmin 150");
 	        panel.add(karten,"left,growx");
 	        panel.add(next,"center,growy,growx");
 	        frame.setResizable(false);
@@ -226,8 +234,21 @@ public class RisikoClientGUI extends JFrame{
     	}catch (SpielerExistiertBereitsException sebe) {
     		 JOptionPane.showMessageDialog(null,sebe.getMessage(),"Name vergeben",JOptionPane.WARNING_MESSAGE);
     	}
+    	if(sp.getSpielerList().size() == anzahl){
+    		try{
+				sp.laenderErstellen();
+				sp.laenderverbindungenUndKontinenteErstellen();
+				sp.missionsListeErstellen();
+			}catch(IOException e){
+				JOptionPane.showMessageDialog(null,e.getMessage(),"Fehler",JOptionPane.WARNING_MESSAGE);
+			}
+			sp.missionenVerteilen();
+			sp.laenderAufteilen();
+			missionen.setText(sp.getSpielerMission(sp.getAktiverSpieler()).getBeschreibung());
+    	}
     	
     }
+
 
 		//spielStarten.addActionListener(starten -> this.risiko(mapWahl.getSelectedItem().toString()));
     class ImagePanel extends JComponent {
