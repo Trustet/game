d023b7975574a3fa6cc5fc9667c925c6a100d825
Trustet <yannik@trustet.de>
2017-06-13 21:40:17 +0200

    Consolasugabe kann jetzt gescrollt werden

--- src/client/ConsolePanel.java
+++ src/client/ConsolePanel.java
@@ -1,14 +1,11 @@
 package client;
 
-import java.awt.Color;
 import java.awt.Font;
 
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.border.Border;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -24,17 +21,18 @@ public class ConsolePanel extends JPanel{
 	
 	public void initialize(){
 //		Border schwarz = BorderFactory.createLineBorder(Color.black);
-		this.setLayout(new MigLayout("wrap1","[640]","[][120]"));
+		this.setLayout(new MigLayout("wrap1","[600]","[][140]"));
 //		this.setBorder(schwarz);
 		
 		JLabel header = new JLabel("Hier stehen irgendwelche Benachrichtigungen, die im Spielverlauf wichtig sind.");
 		JTextArea consoleText = new JTextArea();
-		JScrollBar consoleScrollBar = new JScrollBar();
-		
+		JScrollPane consoleScrollBar = new JScrollPane(consoleText);
+		consoleText.setLineWrap(true);
+		consoleText.setText("Dies \nist \nein\nvorläufiger\ntest\nText\nzum\nscrollen");
 		header.setFont(schrift);
 		consoleText.setFont(schrift);
 		this.add(header,"left, growx");
-		this.add(consoleText,"growx,growy,split2");
-		this.add(consoleScrollBar,"right,growy,split2");
+		this.add(consoleScrollBar,"growx,growy");
+		//this.add(consoleScrollBar,"right,growy,split2");
 	}
 }
